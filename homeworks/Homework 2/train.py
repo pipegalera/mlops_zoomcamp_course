33513b0500c2c92933350bfb1f11254b70837ceb
Pipe Galera <pipegalera@gmail.com>
2024-06-19 23:23:31 +0200

    Updated main

--- homeworks/Homework 2/train.py
+++ homeworks/Homework 2/train.py
@@ -3,8 +3,8 @@ import pickle
 import click
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
-
+from sklearn.metrics import root_mean_squared_error
+import mlflow
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -19,14 +19,20 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
+    # Enable autologging
+    mlflow.set_tracking_uri("http://0.0.0.0:5000/")
+    mlflow.set_experiment("train.py")
+    mlflow.sklearn.autolog()
+
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    
+    with mlflow.start_run():
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
-
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+    rmse = root_mean_squared_error(y_val, y_pred)
 
 
 if __name__ == '__main__':
